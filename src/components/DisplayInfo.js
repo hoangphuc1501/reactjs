df0654c5760aaec79f2e402bed624d14cf7656f1
hoangphuc1501 <katakurilancer.com>
2024-12-01 00:35:35 +0700

    chỉnh logo

--- src/components/DisplayInfo.js
+++ src/components/DisplayInfo.js
@@ -1,6 +1,6 @@
 import React from "react";
 import './DisplayInfo.scss';
-
+import logo from './../logo.svg';
 class DisplayInfo extends React.Component {
     state = {
         isShowListUser: true
@@ -17,6 +17,7 @@ class DisplayInfo extends React.Component {
         return (
 
             <div className="display-infor-container">
+                <img src={logo} alt="" />
                 <div>
                     <span onClick={() => { this.handleShowHide() }}>
                         {this.state.isShowListUser === true ? "Ẩn" : "Hiện"}
